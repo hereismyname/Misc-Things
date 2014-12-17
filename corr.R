914ac40eb669e7aff62d3ec5e84f1da34061439d
Andrew <moore.andrew09@gmail.com>
2014-12-16 19:18:49 -0500

    Added comments

--- corr.R
+++ corr.R
@@ -1,17 +1,19 @@
 corr <- function(directory, threshold = 0) {
+    
+    # directory should be the file containing all specdata files
+    
+    nobs <- c() # empty vector to hold the complete cases for each file
+    corvec <- c()
   
-  nobs <- c() # empty vector to hold the complete cases for each file
-  corvec <- c()
+    specdata_files <- list.files(directory, full.names=TRUE) # make a list of files in "specdata"
+    length <- length(specdata_files)
   
-  specdata_files <- list.files(directory, full.names=TRUE) # make a list of files in "specdata"
-  length <- length(specdata_files)
-  
-  for (i in 1:length) {
-    spec <- read.csv(specdata_files[i]) # gather data from specdata
-    specnobs <- sum(complete.cases(spec)) # check complete cases
+    for (i in 1:length) {
+        spec <- read.csv(specdata_files[i]) # gather data from specdata
+        specnobs <- sum(complete.cases(spec)) # check complete cases
     if (specnobs > threshold) {
-      polucor <- cor(spec$nitrate, spec$sulfate, use="pairwise.complete.obs")
-      corvec <- c(corvec, polucor)
+        polucor <- cor(spec$nitrate, spec$sulfate, use="pairwise.complete.obs")
+        corvec <- c(corvec, polucor)
     }
     nobs <- c(nobs, specnobs)
   }
