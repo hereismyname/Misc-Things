81d6865cda18b58a5cb6919847596718ed243439
Andrew <moore.andrew09@gmail.com>
2014-12-16 19:16:13 -0500

    Added an old file from R-Prog class

---
+++ corr.R
@@ -0,0 +1,19 @@
+corr <- function(directory, threshold = 0) {
+  
+  nobs <- c() # empty vector to hold the complete cases for each file
+  corvec <- c()
+  
+  specdata_files <- list.files(directory, full.names=TRUE) # make a list of files in "specdata"
+  length <- length(specdata_files)
+  
+  for (i in 1:length) {
+    spec <- read.csv(specdata_files[i]) # gather data from specdata
+    specnobs <- sum(complete.cases(spec)) # check complete cases
+    if (specnobs > threshold) {
+      polucor <- cor(spec$nitrate, spec$sulfate, use="pairwise.complete.obs")
+      corvec <- c(corvec, polucor)
+    }
+    nobs <- c(nobs, specnobs)
+  }
+  corvec
+}
